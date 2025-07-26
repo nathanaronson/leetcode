9fd5b846fd7275bbb95974ebeef4d858d7495a30
Nathan Aronson <njaronson421@gmail.com>
2025-07-26 10:45:05 -0400

    242-valid-anagram

---
+++ easy/242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // If they are different sizes, they cannot be anagrams.
+        if(s.size() != t.size()) return false;
+
+        int freq[26] = {0};
+
+        // Take the difference in frequency of each character.
+        for(int i = 0; i < s.size(); ++i) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+
+        // If that difference is non-zero, they are not anagrams.
+        for(int i : freq) {
+            if(i != 0) return false;
+        }
+
+        // If all characters have the same frequency, they are anagrams.
+        return true;
+    }
+};
